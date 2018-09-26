30856ca44b3bc9b4755ff17b93af7e358f6d047a
miyamoto yuji <mymt.yuji0612@gmail.com>
2018-09-27 08:55:03 +0900

    add AWS service

---
+++ src/appConfig.js
@@ -0,0 +1,7 @@
+const appConfig = {
+  region: 'ap-northeast-1',
+  IdentityPoolId: 'ap-northeast-1:xxxxxxxxxxxxxxxxx',
+  UserPoolId: 'ap-northeast-1_xxxxxxxxxx',
+  ClientId: 'xxxxxxxxxxxxxx',
+};
+export default appConfig;
